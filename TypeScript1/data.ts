5feaae90cd0aa599701964a0a747bbf3e2b5d46f
Maria Lu <m.benavidesd@uniandes.edu.co>
2024-10-20 15:56:28 -0500

    tabla simple de series :D

---
+++ TypeScript1/data.ts
@@ -0,0 +1,10 @@
+import { Serie } from './serie';
+
+export const series: Serie[] = [
+    new Serie(1, 'Stranger Things', 'Netflix', 4),
+    new Serie(2, 'Breaking Bad', 'AMC', 5),
+    new Serie(3, 'The Crown', 'Netflix', 6),
+    new Serie(4, 'The Mandalorian', 'Disney+', 3),
+    new Serie(5, 'Game of Thrones', 'HBO', 8),
+    new Serie(6, 'The Office', 'NBC', 9),
+];
